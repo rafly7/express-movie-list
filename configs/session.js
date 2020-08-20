68c2e7407d3c06735442877d3480c439112c4e1a
rafly7 <raflydipoe@gmail.com>
2020-08-20 10:31:04 +0700

    [UPDATE] remove file admin SDK Firebase and change maxAge cookie to 1 hour

--- configs/session.js
+++ configs/session.js
@@ -6,7 +6,7 @@ export const SESSION_OPTION = {
   secret: process.env.SESSION_SECRET,
   name: process.env.SESSION_NAME,
   cookie: {
-    maxAge: 1000 * 60 * 60 / 2,
+    maxAge: 1000 * 60 * 60,
     secure: process.env.NODE_ENV === 'production',
     sameSite: true,
   },
